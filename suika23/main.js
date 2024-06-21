3881fbcd4e45681a0b3c62e0109214bcfc94f75d
HONGEUNTAEK <hongeuntaek09@gmail.com>
2024-06-21 11:02:45 +0900

    aassa

--- suika23/main.js
+++ suika23/main.js
@@ -1,3 +1,5 @@
+import { FRUITS } from "./fruits.js";
+
 var Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
@@ -13,7 +15,7 @@ const render = Render.create({
     element : document.body,
     options : {
         wireframes : false,
-        background : '#d89fd9',
+        background : '#ffe380',
         width: 620,
         height : 850,
     }
@@ -38,13 +40,45 @@ const ground = Bodies.rectangle(310, 820, 620, 60, {
     render : { fillStyle: '#E6B143'}
 });
 
-const topwall = Bodies.rectangle(15, 395, 30, 790,2, {
+
+const topLine = Bodies.rectangle(310, 150, 620, 2, {
     isStatic : true,
+    isSensor : true,
     render : { fillStyle: '#E6B143'}
 });
 
 // 벽 월드에 배치
-World.add(world, [leftWall,rightWall,ground,topwall]);
+World.add(world, [leftWall, rightWall, ground, topLine]);
 
 Render.run(render);
 Runner.run(engine);
+
+let currentBody = null;
+let currentFruit = null;
+
+function addFruit() {
+
+    // 과일 인덱스값 저장
+    const index = Math.floor(Math.random() * 5);
+
+    // 과일 이미지 경로 불러오기 변수
+    const fruit = FRUITS[index];
+
+    const body = Bodies.circle(300, 50, fruit.radius, {
+        index : index,
+        isSleeping : true, // 시작시 바로 떨어지지 않고 잠시 멈춤
+        render : {
+            sprite : { texture : `${fruit.name}.png`},
+        },
+
+        // 통통 튀기는 설정
+        restitution : 0.3
+    })
+
+    currentBody = body;
+    currentFruit = fruit;
+
+    World.add(world, body)
+}
+
+addFruit();
